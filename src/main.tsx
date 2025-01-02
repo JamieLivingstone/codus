4a5ee2245587c583cc1126a0b5a549950b0926e6
Jamie Livingstone <jamie.livingstone@outlook.com>
2025-01-02 12:52:10 +0000

    feat: setup mantime and create custom theme

--- src/main.tsx
+++ src/main.tsx
@@ -1,9 +1,36 @@
+import { MantineProvider, createTheme } from '@mantine/core';
 import React from 'react';
 import ReactDOM from 'react-dom/client';
-import App from './App';
+import '@mantine/core/styles.css';
+
+const theme = createTheme({
+  primaryColor: 'blue',
+  defaultRadius: 'md',
+  colors: {
+    dark: [
+      '#C1C2C5',
+      '#A6A7AB',
+      '#909296',
+      '#5C5F66',
+      '#373A40',
+      '#2C2E33',
+      '#222429',
+      '#1A1B1E',
+      '#141517',
+      '#101113',
+    ],
+  },
+  defaultGradient: {
+    from: 'blue',
+    to: 'cyan',
+    deg: 45,
+  },
+});
 
 ReactDOM.createRoot(document.getElementById('root') as HTMLElement).render(
   <React.StrictMode>
-    <App />
+    <MantineProvider theme={theme}>
+      <h1>Hello World</h1>
+    </MantineProvider>
   </React.StrictMode>,
 );
